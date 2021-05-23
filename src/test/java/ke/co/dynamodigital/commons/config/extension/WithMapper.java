327cd8ca4e66796ea1e4c0c14796ca10f7bf3462
Bibibiu <arthurmita@gmail.com>
2021-05-23 17:06:15 +0300

    fixed: made feign classes public

--- src/test/java/ke/co/dynamodigital/commons/config/extension/WithMapper.java
+++ src/test/java/ke/co/dynamodigital/commons/config/extension/WithMapper.java
@@ -1,6 +1,6 @@
 package ke.co.dynamodigital.commons.config.extension;
 
-import com.github.rozidan.springboot.modelmapper.WithModelMapper;
+import ke.co.dynamodigital.commons.config.annotations.WithModelMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
